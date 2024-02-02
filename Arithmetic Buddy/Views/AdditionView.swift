bf417e8a6ec1c952cf3528f415ac0a7d5d500cb3
Xinlin Zhou <XZhou@lcs.on.ca>
2024-02-02 09:10:50 -0500

    Created the project and finished initial project organization.

---
+++ Arithmetic Buddy/Views/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  Arithmetic Buddy
+//
+//  Created by Fiona ZHOU on 2024-02-02.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
